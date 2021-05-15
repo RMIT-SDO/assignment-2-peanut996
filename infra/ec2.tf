a18069ae871b4b3ed2ed7c13ccbc305d90afbf12
Peanuts <849421294godw@gmail.com>
2021-05-15 15:24:42 +0000

    init repo

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "rmit-assignment-2-key"
-  public_key = file("/path/to/ssh/key")
-}
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "rmit-assignment-2-key"
+#   public_key = file("/path/to/ssh/key")
+# }
